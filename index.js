57d4eefe064a037fdeddb0e6c71981611f4549a4
vlopez <vmlopez@slashweb.com.mx>
2020-06-22 15:52:05 -0500

    Mensaje de prueba

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import { WebView } from 'react-native-webview';
 import {Button} from 'react-native-elements';
 import { CreditCardInput } from 'react-native-credit-card-input';
